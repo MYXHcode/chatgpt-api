856692425e14b5915e9f4ede111453af47e92f7c
MYXH <1735350920@qq.com>
2024-02-01 17:07:36 +0800

    《ChatGPT 微服务应用体系构建》 - chatgpt-api 第3节：微信公众号验签和初步对接OpenAI

--- src/main/java/com/myxh/chatgpt/domain/receive/model/MessageTextEntity.java
+++ src/main/java/com/myxh/chatgpt/domain/receive/model/MessageTextEntity.java
@@ -1,95 +1,83 @@
 package com.myxh.chatgpt.domain.receive.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.Getter;
+
 /**
  * @author MYXH
  * @date 2023/12/13
+ * @description 消息文本实体
+ * @GitHub <a href="https://github.com/MYXHcode">MYXHcode</a>
  */
-
-import com.thoughtworks.xstream.annotations.XStreamAlias;
-
-public class MessageTextEntity {
-
+@Getter
+public class MessageTextEntity
+{
     @XStreamAlias("MsgId")
     private String msgId;
+
     @XStreamAlias("ToUserName")
     private String toUserName;
+
     @XStreamAlias("FromUserName")
     private String fromUserName;
+
     @XStreamAlias("CreateTime")
     private String createTime;
+
     @XStreamAlias("MsgType")
     private String msgType;
+
     @XStreamAlias("Content")
     private String content;
+
     @XStreamAlias("Event")
     private String event;
+
     @XStreamAlias("EventKey")
     private String eventKey;
 
-    public MessageTextEntity() {
+    public MessageTextEntity()
+    {
+
     }
 
-    public String getMsgId() {
-        return msgId;
-    }
-
-    public void setMsgId(String msgId) {
+    public void setMsgId(String msgId)
+    {
         this.msgId = msgId;
     }
 
-    public String getToUserName() {
-        return toUserName;
-    }
-
-    public void setToUserName(String toUserName) {
+    public void setToUserName(String toUserName)
+    {
         this.toUserName = toUserName;
     }
 
-    public String getFromUserName() {
-        return fromUserName;
-    }
-
-    public void setFromUserName(String fromUserName) {
+    public void setFromUserName(String fromUserName)
+    {
         this.fromUserName = fromUserName;
     }
 
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
+    public void setCreateTime(String createTime)
+    {
         this.createTime = createTime;
     }
 
-    public String getMsgType() {
-        return msgType;
-    }
-
-    public void setMsgType(String msgType) {
+    public void setMsgType(String msgType)
+    {
         this.msgType = msgType;
     }
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
+    public void setContent(String content)
+    {
         this.content = content;
     }
 
-    public String getEvent() {
-        return event;
-    }
-
-    public void setEvent(String event) {
+    public void setEvent(String event)
+    {
         this.event = event;
     }
 
-    public String getEventKey() {
-        return eventKey;
-    }
-
-    public void setEventKey(String eventKey) {
+    public void setEventKey(String eventKey)
+    {
         this.eventKey = eventKey;
     }
 }
